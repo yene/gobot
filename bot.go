360fb7ec91bc28aafecd5f1931496786c0cdc128
yene <yene@cocoadojo.com>
2014-08-08 19:33:35 +0200

    removed streamer again

--- bot.go
+++ bot.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	channel := "#test" //#r/dota2"
-	con := irc.IRC("7kMMR", "7kMMR")
+	channel := "#r/dota2" //#r/dota2"
+	con := irc.IRC("Lina", "Lina")
 	err := con.Connect("irc.quakenet.org:6667")
 	if err != nil {
 		fmt.Println("Failed connecting")
@@ -20,8 +20,8 @@ func main() {
 	con.AddCallback("PRIVMSG", func(e *irc.Event) {
 
 		switch e.Message() {
-		case "!matches", "!m":
-			con.Privmsg(channel, "no matches")
+		//case "!matches", "!m":
+		//con.Privmsg(channel, "no matches")
 		case "!help":
 			con.Privmsg(channel, "no help")
 		case "!scores":
@@ -34,8 +34,13 @@ func main() {
 			for _, g := range streamer.TopDota2Streams() {
 				con.Privmsg(channel, g)
 			}
+		case "!relax":
+			con.Privmsg(channel, "People forget that they also win games because the enemies are feeding and are shit.")
+			// con.Privmsg(channel, "MMR is not bugged or rigged or anything. Get better, you'll end up with better MMR.")
+			// con.Privmsg(channel, "Stop crying that "I ended up with 2500 because my team mates suck". No. You ended up 2,5k because you suck. ")
+		}
 		case "!joke":
-			con.Privmsg(channel, "my mmr")
+			con.Privmsg(channel, "my MMR")
 		}
 	})
 	con.Loop()
--- streamer/README.md
+++
@@ -1,7 +0,0 @@
-Streamer
-====
-
-a little package that check who is streaming on twitch
-
-source
-* https://github.com/justintv/twitch-api
